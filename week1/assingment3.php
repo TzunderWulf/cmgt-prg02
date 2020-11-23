4a90e4977b723197ed819f6c56267be454469a14
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-11-23 18:27:55 +0100

    Update assingment3.php

--- week1/assingment3.php
+++ week1/assingment3.php
@@ -2,33 +2,33 @@
 $musicAlbums = [
     "1" => ["artistName" => "Panic! at the Disco",
             "albumName" => "A Fever You Can't Sweat Out",
-            "releaseYear" => 2005,
-            "tracksAmount" => 13
+            "releaseYear" => '2005',
+            "tracksAmount" => '13'
            ],
     "2" => ["artistName" => "Panic! at the Disco",
             "albumName" => "Pretty. Odd.",
-            "releaseYear" => 2008,
-            "tracksAmount" =>15
+            "releaseYear" => '2008',
+            "tracksAmount" => '15'
            ],
     "3" => ["artistName" => "Panic! at the Disco",
             "albumName" => "Vices & Virtues",
-            "releaseYear" => 2011,
-            "tracksAmount" => 10
+            "releaseYear" => '2011',
+            "tracksAmount" => '10'
            ],
     "4" => ["artistName" => "Panic! at the Disco",
             "albumName" => "Too Weird to Live, Too Rare to Die!",
-            "releaseYear" => 2013,
-            "tracksAmount" =>10
+            "releaseYear" => '2013',
+            "tracksAmount" => '10'
            ],
     "5" => ["artistName" => "Panic! at the Disco",
             "albumName" => "Death of a Bachelor",
-            "releaseYear" => 2016,
-            "tracksAmount" => 11
+            "releaseYear" => '2016',
+            "tracksAmount" => '11'
            ],
     "6" => ["artistName" => "Panic! at the Disco",
             "albumName" => "Pray for the Wicked",
-            "releaseYear" => 2018,
-            "tracksAmount" => 11
+            "releaseYear" => '2018',
+            "tracksAmount" => '11'
     ]
 ];
 ?>
