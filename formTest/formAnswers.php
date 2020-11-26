e1b11b9db9b05695e72d9cccad409a98f9112d00
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-11-26 21:11:13 +0100

    added form validation and works

---
+++ formTest/formAnswers.php
@@ -0,0 +1,47 @@
+<?php
+    # define variables and set to empty values
+    $name = $email = $gender = $comment = $website = "";
+    $nameErr = $emailErr = $genderErr = $websiteErr = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "* Name is required";
+        } else {
+            $name = test_input($_POST["name"]);
+        }
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+?>
+
+<html>
+    <head>
+        <title>Form validation</title>
+    </head>
+
+    <body>
+        <h2>Form validation in PHP</h2>
+        <p>I can now validate forms in PHP.</p>
+
+        <p>* represent a required field</p>
+
+        <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+            <label for="name">* Name: </label>
+            <input type="text" name="name" id="name">
+            <p class="error"><?= $nameErr;?></p>
+
+            <input type="submit">
+        </form>
+
+        <style>
+            .error {
+                color: red;
+            }
+        </style>
+    </body>
+</html>
