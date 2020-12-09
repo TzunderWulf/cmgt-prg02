02125e3bb92ee44fd534fd59f95d09a85667a89b
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-12-09 02:24:41 +0100

    testing validation licenseplates

--- testingFormValidation/testvalidationlicenseplate.php
+++ testingFormValidation/testvalidationlicenseplate.php
@@ -1,22 +1,22 @@
 <?php
-
 $licensePlate = '';
 $licensePlateErr = '';
 
 // valid license plate pattern in the Netherlands
 $validLicensePlates = [
-    "/^([A-Z]{2})(\d{2})(\d{2})/i", // XX-99-99
-    "/^(\d{2})(\d{2})([A-Z]{2})/i", // 99-99-XX
-    "/^(\d{2})([A-Z]{2})(\d{2})/i", // 99-XX-99
-    // "/^([A-Z]{2})(\d{2})([A-Z]{2})/i", // XX-99-XX
+    "/([A-Z]{2})(\d{2})(\d{2})/i", // XX-99-99
+    "/(\d{2})(\d{2})([A-Z]{2})/i", // 99-99-XX
+    "/(\d{2})([A-Z]{2})(\d{2})/i", // 99-XX-99
+    "/([A-Z]{2})(\d{2})([A-Z]{2})/i", // XX-99-XX
     // "/^([A-Z]{2})([A-Z]{2})(\d{2})/i", // XX-XX-99
-    "/^(\d{2})([A-Z]{2})([A-Z]{2})/i", // 99-XX-XX
-    "/^(\d{2})([A-Z]{3})(\d)/i", // 99-XXX-9
-    "/^(\d)([A-Z]{3})(\d{2})/i", // 9-XXX-99
+    "/(\d{2})([A-Z]{2})([A-Z]{2})/i", // 99-XX-XX
+    "/(\d{2})([A-Z]{3})(\d)/i", // 99-XXX-9
+    "/(\d)([A-Z]{3})(\d{2})/i", // 9-XXX-99
     // "/^([A-Z]{2})(\d{3})([A-Z])/i", // XX-999-X
     // "/^([A-Z])(\d{3})([A-Z]{2})/i", // X-999-XX
     // "/^([A-Z]{3})(\d{2})([A-Z])/i" // XXX-99-X
 ];
+$validForm = true;
 
 foreach ($validLicensePlates as $validLicensePlate) { // checks for each different pattern
     $licensePlateCheck = preg_replace('/-/', '', $_POST['licensePlate']); // removes -
@@ -24,15 +24,15 @@ foreach ($validLicensePlates as $validLicensePlate) { // checks for each differe
     // part which checks the matches
     if ($licensePlateCheck == $nonValidPattern) {
         $validForm = false;
+        echo $validForm;
         $licensePlateErr = 'Geen geldig kenteken ingevoerd.';
     }
     break;
 }
-// print_r($validLicensePlates);
-if ($licensePlateErr == '') {
-    $licensePlate = htmlspecialchars($_POST['licensePlate']);
-}
+echo $validForm;
+echo preg_match($validLicensePlate, $licensePlateCheck);
 ?>
+
 <form action="" method="post">
 <label for="kenteken">Kenteken*: </label>
 <input type="text" id="kenteken" name="licensePlate" maxlength="8" value="<?=$licensePlate?>">
